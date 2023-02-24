b6e2464191765ab54cb7954fab488a6ef1e7fc32
Ilya Skripnikov <scripnikovs@list.ru>
2023-02-24 20:30:00 +0300

    Added New project

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int ElementCount(string[] array, int n)
+{
+  int count = 0;
+  foreach (string element in array)
+  {
+    if (element.Length <= n)
+      count++;
+  }
+  return count;
+}
+
+string[] NewString(string[] array, int k)
+{
+  int n = ElementCount(array, k);
+  string[] newArray = new string[n];
+  int index = 0;
+  foreach (string element in array)
+  {
+    if (element.Length <= k)
+    {
+      newArray[index] = element;
+      index++;
+    }
+  }
+  return newArray;
+}
+
+void InputArray(string[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+    array[i] = Convert.ToString(Console.ReadLine());
+}
+
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[n];
+Console.WriteLine("Введите элементы массива: ");
+InputArray(array);
+int m = ElementCount(array, 3);
+Console.WriteLine($"Итоговый массив: [{string.Join(" ", NewString(array, 3))}]");
+
+
